b0d03a17642e12342aa9371a830afb910778302f
olegtkach <olegontkach101@gmail.com>
2025-05-12 23:19:39 +0300

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import React, { Suspense } from "react";
+
 import vehicles from "@/mock/mock_vehicles.json";
 import ItemsTable from "@/components/iItems-table";
 import Filters from "@/components/filters";
@@ -5,8 +7,10 @@ import Filters from "@/components/filters";
 export default async function Page() {
   return (
     <>
-      <Filters items={vehicles} />
-      <ItemsTable items={vehicles} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Filters items={vehicles} />
+        <ItemsTable items={vehicles} />
+      </Suspense>
     </>
   );
 }
